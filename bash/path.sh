e3461cc276c32171cffe0e103e560fc650b76f96
Corey Csuhta <git@cjcsuhta.com>
2013-02-24 22:28:35 -0500

    Add settings for rbenv

--- bash/path.sh
+++ bash/path.sh
@@ -1,15 +1,15 @@
 # Custom $PATH
 
-if [[ (-e /usr/local/bin/gem && -e /usr/local/bin/brew) ]]; then
-  export GEM_BIN_PATH=`/usr/local/bin/brew info ruby|grep /bin|tr -d ' '`
-else
-  export GEM_BIN_PATH=""
-fi
-
+# export GEM_BIN_PATH="/usr/local/opt/ruby/bin"
 export HEROKU_PATH="/usr/local/heroku/bin"
 export PG_PATH="/Applications/Postgres.app/Contents/MacOS/bin"
 export PYTHON_PATH="/usr/local/share/python"
 export NODE_PATH="/usr/local/share/npm/bin"
 export OSX_PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin"
 
-export PATH="$HEROKU_PATH:$PG_PATH:$GEM_BIN_PATH:$NODE_PATH:$PYTHON_PATH:$OSX_PATH"
+export PATH="$HEROKU_PATH:$PG_PATH:$NODE_PATH:$PYTHON_PATH:$OSX_PATH"
+
+# Activate rbenv
+
+export RBENV_ROOT="/usr/local/var/rbenv"
+if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
