26b9e493e5c18087e1fa3fbe189dbd5ad6392429
Corey Csuhta <git@cjcsuhta.com>
2013-06-17 10:34:01 -0400

    Update path and add new tool script

--- bash/path.sh
+++ bash/path.sh
@@ -1,12 +1,13 @@
 # Custom $PATH
 
+export USR_PATH="/usr/local/bin:/usr/local/sbin"
 export HEROKU_PATH="/usr/local/heroku/bin"
 export PG_PATH="/Applications/Postgres.app/Contents/MacOS/bin"
 export PYTHON_PATH="/usr/local/share/python"
 export NODE_PATH="/usr/local/share/npm/bin"
-export OSX_PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin"
+export OSX_PATH="/usr/bin:/usr/sbin:/bin:/sbin"
 
-export PATH="$HEROKU_PATH:$PG_PATH:$NODE_PATH:$PYTHON_PATH:$OSX_PATH"
+export PATH="$USR_PATH:$HEROKU_PATH:$PG_PATH:$NODE_PATH:$PYTHON_PATH:$OSX_PATH"
 
 # Activate rbenv
 export RBENV_ROOT="/usr/local/var/rbenv"
