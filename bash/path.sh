ba77481aed56a06f2e1b815af0e40fce831ffa50
Corey Csuhta <git@cjcsuhta.com>
2014-02-14 17:56:15 -0500

    Add ./bin to $PATH

--- bash/path.sh
+++ bash/path.sh
@@ -1,13 +1,14 @@
 # Custom $PATH
 
-export USR_PATH="/usr/local/bin:/usr/local/sbin"
+export USR_PATH="./bin:/usr/local/bin:/usr/local/sbin"
 export HEROKU_PATH="/usr/local/heroku/bin"
-export PG_PATH="/Applications/Postgres.app/Contents/MacOS/bin:/Applications/Postgres93.app/Contents/MacOS/bin"
+export PG_PATH="/Applications/Postgres.app/Contents/MacOS/bin:/Applications/Postgres.app/Contents/MacOS/bin"
 export NODE_PATH="/usr/local/share/npm/bin"
 export OSX_PATH="/usr/bin:/usr/sbin:/bin:/sbin"
 
 export PATH="$USR_PATH:$HEROKU_PATH:$PG_PATH:$NODE_PATH:$OSX_PATH"
 
 # Activate rbenv
+
 export RBENV_ROOT="/usr/local/var/rbenv"
 if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
