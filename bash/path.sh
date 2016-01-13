4ece01a3a293a707ddfb78436d8950ff0ba083d1
Corey Csuhta <git@cjcsuhta.com>
2016-01-13 14:05:37 -0500

    Postgres 9.5

--- bash/path.sh
+++ bash/path.sh
@@ -2,12 +2,13 @@
 
 export USR_PATH="/usr/local/bin:/usr/local/sbin"
 export HEROKU_PATH="/usr/local/heroku/bin"
-export PG_PATH="/Applications/Postgres.app/Contents/Versions/9.4/bin"
+export PG_PATH="/Applications/Postgres.app/Contents/Versions/9.5/bin"
 export NODE_PATH="/usr/local/share/npm/bin"
 export OSX_PATH="/usr/bin:/usr/sbin:/bin:/sbin"
 
 export PATH="$USR_PATH:$HEROKU_PATH:$PG_PATH:$NODE_PATH:$OSX_PATH"
 
 # Activate chruby and the .ruby-version auto-switcher
+
 source /usr/local/opt/chruby/share/chruby/chruby.sh
 source /usr/local/opt/chruby/share/chruby/auto.sh
