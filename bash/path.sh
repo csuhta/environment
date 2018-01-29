4bf5034128ca440965de402b0def5b870249757c
Corey Csuhta <git@cjcsuhta.com>
2018-01-29 15:26:00 -0500

    Remove $HEROKU_PATH, add $CDPATH

--- bash/path.sh
+++ bash/path.sh
@@ -1,13 +1,13 @@
 # Custom $PATH
 
 export USR_PATH="/usr/local/bin:/usr/local/sbin"
-export HEROKU_PATH="/usr/local/heroku/bin"
 export PG_PATH="/Applications/Postgres.app/Contents/Versions/latest/bin"
 export NODE_PATH="/usr/local/share/npm/bin"
 export OSX_PATH="/usr/bin:/usr/sbin:/bin:/sbin"
 export GPG_PATH="/usr/local/MacGPG2/bin"
 
-export PATH="$HEROKU_PATH:$PG_PATH:$GPG_PATH:$USR_PATH:$NODE_PATH:$OSX_PATH"
+export PATH="$PG_PATH:$GPG_PATH:$USR_PATH:$NODE_PATH:$OSX_PATH"
+export CDPATH=".:~:~/Projects"
 
 # Activate chruby and the .ruby-version auto-switcher
 
